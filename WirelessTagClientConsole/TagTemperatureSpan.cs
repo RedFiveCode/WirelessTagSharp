5591d2760fac4950e77ff532e5adc25b732d6f9d
John <88855545+RedFiveCode@users.noreply.github.com>
2022-08-20 17:00:32 +0100

    Tidy up async client; show recent temperature for last week, month etc.

---
+++ WirelessTagClientConsole/TagTemperatureSpan.cs
@@ -0,0 +1,11 @@
+﻿using WirelessTagClientLib.DTO;
+
+namespace WirelessTagClientConsole
+{
+    internal class TagTemperatureSpan
+    {
+        public TagInfo Tag { get; set; }
+        public double Min { get; set; }
+        public double Max { get; set; }
+    }
+}
