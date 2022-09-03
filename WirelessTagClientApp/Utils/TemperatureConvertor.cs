f5bb8f3472d9a8e816a917432d60f8ac4ec2c1a3
John <John@Nectarine>
2022-09-03 18:28:35 +0100

    Add temperature converter to convert °C to °F

---
+++ WirelessTagClientApp/Utils/TemperatureConvertor.cs
@@ -0,0 +1,15 @@
+﻿namespace WirelessTagClientApp.Utils
+{
+    public class TemperatureConvertor
+    {
+        /// <summary>
+        /// Convert °C to °F.
+        /// </summary>
+        /// <param name="celcius"></param>
+        /// <returns></returns>
+        public static double ConvertToFarenheit(double celcius)
+        {
+            return 32d + ((9d * celcius) / 5d);
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Utils/TemperatureConvertorTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WirelessTagClientApp.Utils;
+
+namespace WirelessTagClientApp.Test.Utils
+{
+    [TestClass]
+    public class TemperatureConvertorTest
+    {
+        [TestMethod]
+        public void Convert_0_Should_Return_32()
+        {
+            // act
+            var result = TemperatureConvertor.ConvertToFarenheit(0d);
+
+            // assert
+            Assert.AreEqual(32d, result);
+        }
+
+        [TestMethod]
+        public void Convert_100_Should_Return_212()
+        {
+            // act
+            var result = TemperatureConvertor.ConvertToFarenheit(100d);
+
+            // assert
+            Assert.AreEqual(212d, result);
+        }
+    }
+}
