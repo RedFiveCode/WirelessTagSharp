dc72caeb3835d11850752c38a10da4ef22337250
John <John@Nectarine>
2022-09-13 07:34:32 +0100

    Make main window host active view control

---
+++ WirelessTagClientApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Input;
+using WirelessTagClientApp.Commands;
+using WirelessTagClientApp.Common;
+using WirelessTagClientLib;
+
+namespace WirelessTagClientApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly IWirelessTagAsyncClient client;
+        private readonly Options options;
+
+        private DateTime lastUpdated;
+        private bool isBusy;
+        private bool isError;
+        private string errorMessage;
+
+        private RefreshCommand refreshCommand;
+        private CloseCommand closeCommand;
+        // need new refresh command that will push a message on the bus to tell the active view to refresh itself???
+
+        private AllTagsViewModel activeViewModel;
+
+        /// <summary>
+        /// Ctor for unit testing
+        /// </summary>
+        public MainWindowViewModel() : this(null, null)
+        { }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="options"></param>
+        public MainWindowViewModel(Options options) : this(new WirelessTagAsyncClient(), options)
+        { }
+
+        /// <summary>
+        /// Ctor for unit testing and common overload.
+        /// </summary>
+        /// <param name="client"></param>
+        public MainWindowViewModel(IWirelessTagAsyncClient client, Options options)
+        {
+            this.client = client;
+            this.options = options;
+            isBusy = false;
+            isError = false;
+            errorMessage = String.Empty;
+
+            refreshCommand = new RefreshCommand();
+            closeCommand = new CloseCommand();
+            activeViewModel = new AllTagsViewModel(this.options);
+        }
+
+        // Refresh the active view
+        public void Refresh()
+        {
+            activeViewModel.Refresh();
+
+            LastUpdated = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Get/set the time last updated from the server
+        /// </summary>
+        public DateTime LastUpdated
+        {
+            get { return lastUpdated; }
+            set
+            {
+                lastUpdated = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Get/set the busy flag
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Get/set the error flag
+        /// </summary>
+        public bool IsError
+        {
+            get { return isError; }
+            set
+            {
+                isError = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Get/set the error message
+        /// </summary>
+        /// <remarks>
+        /// Sets/resets the <see cref="IsError"/> property
+        /// </remarks>
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set
+            {
+                errorMessage = value;
+
+                IsError = !String.IsNullOrEmpty(value);
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Get the command to refresh the tag data
+        /// </summary>
+        public ICommand RefreshCommand
+        {
+            get { return refreshCommand.Command; }
+        }
+
+        /// <summary>
+        /// Get the command to close the app
+        /// </summary>
+        public ICommand CloseCommand
+        {
+            get { return closeCommand.Command; }
+        }
+
+        public AllTagsViewModel ActiveViewModel
+        {
+            get { return activeViewModel; }
+        }
+
+    }
+}
---
+++ WirelessTagClientAppTest/ViewModel/MainWindowViewModelTest.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel;
+using WirelessTagClientApp.ViewModels;
+using WirelessTagClientAppTest.TestHelpers;
+
+namespace WirelessTagClientApp.Test.ViewModel
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        [TestMethod]
+        public void Class_Should_Implement_INotifyPropertyChanged()
+        {
+            // act
+            var target = new MainWindowViewModel();
+
+            // assert
+            Assert.IsInstanceOfType(target, typeof(INotifyPropertyChanged));
+        }
+
+        [TestMethod]
+        public void Ctor_Should_Initialise_Properties_To_Expected_Values()
+        {
+            // act
+            var target = new MainWindowViewModel();
+
+            // assert
+            Assert.AreEqual(DateTime.MinValue, target.LastUpdated);
+            Assert.IsFalse(target.IsBusy);
+            Assert.IsFalse(target.IsError);
+            Assert.AreEqual(String.Empty, target.ErrorMessage);
+
+            // commands
+            Assert.IsNotNull(target.CloseCommand);
+
+            // view-model(s)
+            Assert.IsNotNull(target.ActiveViewModel);
+        }
+
+        [TestMethod]
+        public void LastUpdated_Setter_Should_Fire_PropertyChanged_Event()
+        {
+            // arrange
+            var target = new MainWindowViewModel();
+            var observer = new PropertyChangedObserver(target);
+
+            // act
+            target.LastUpdated = DateTime.Now;
+
+            // assert
+            observer.AssertPropertyChangedEvent("LastUpdated");
+        }
+
+        [TestMethod]
+        public void IsBusy_Setter_Should_Fire_PropertyChanged_Event()
+        {
+            // arrange
+            var target = new MainWindowViewModel();
+            var observer = new PropertyChangedObserver(target);
+
+            // act
+            target.IsBusy = true;
+
+            // assert
+            observer.AssertPropertyChangedEvent("IsBusy");
+        }
+
+        [TestMethod]
+        public void IsError_Setter_Should_Fire_PropertyChanged_Event()
+        {
+            // arrange
+            var target = new MainWindowViewModel();
+            var observer = new PropertyChangedObserver(target);
+
+            // act
+            target.IsError = true;
+
+            // assert
+            observer.AssertPropertyChangedEvent("IsError");
+        }
+
+        [TestMethod]
+        public void ErrorMessage_Setter_Should_Fire_PropertyChanged_Events()
+        {
+            // arrange
+            var target = new MainWindowViewModel();
+            var observer = new PropertyChangedObserver(target);
+
+            // act
+            target.ErrorMessage = "Error message";
+
+            // assert
+            observer.AssertPropertyChangedEvent("ErrorMessage");
+            observer.AssertPropertyChangedEvent("IsError");
+        }
+
+        [TestMethod]
+        public void ErrorMessage_Setter_Should_Set_IsError_Property()
+        {
+            // arrange
+            var target = new MainWindowViewModel();
+            var observer = new PropertyChangedObserver(target);
+
+            // act
+            target.ErrorMessage = "Error message";
+
+            // assert
+           Assert.IsTrue(target.IsError);
+        }
+
+        [TestMethod]
+        public void ErrorMessage_Setter_Should_Reset_IsError_Property()
+        {
+            // arrange
+            var target = new MainWindowViewModel();
+            var observer = new PropertyChangedObserver(target);
+
+            // act
+            target.ErrorMessage = "";
+
+            // assert
+            Assert.IsFalse(target.IsError);
+        }
+    }
+}
