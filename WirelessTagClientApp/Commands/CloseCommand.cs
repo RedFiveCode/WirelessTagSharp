95b00ea33d24be7de8254936fa47d05afe2c0438
John <88855545+RedFiveCode@users.noreply.github.com>
2022-09-13 07:23:45 +0100

    Add context menu and Close command

---
+++ WirelessTagClientApp/Commands/CloseCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WirelessTagClientApp.Common;
+
+namespace WirelessTagClientApp.Commands
+{
+    /// <summary>
+    /// Command to close the app.
+    /// </summary>
+    public class CloseCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloseCommand"/> class.
+        /// </summary>
+        public CloseCommand()
+        {
+            Command = new RelayCommandT<object>(p => Close(p));
+        }
+
+        private void Close(object o)
+        {
+             Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Get the command object.
+        /// </summary>
+        public ICommand Command { get; private set; }
+    }
+}
