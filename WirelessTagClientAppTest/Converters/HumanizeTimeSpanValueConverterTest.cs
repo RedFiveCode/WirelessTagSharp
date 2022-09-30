c6816e8f6277b050e806ef6a3228721a5bd886b7
John <John@Nectarine>
2022-09-30 21:05:39 +0100

    Add unit tests for HumanizeTimeSpanValueConverter class

---
+++ WirelessTagClientAppTest/Converters/HumanizeTimeSpanValueConverterTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Data;
+using WirelessTagClientApp.Converters;
+
+namespace WirelessTagClientAppTest.Converters
+{
+    /// <summary>
+    /// Unit tests for the <see cref="HumanizeTimeSpanValueConverter"/> class
+    /// </summary>
+    [TestClass]
+    public class HumanizeTimeSpanValueConverterTest
+    {
+        [TestMethod]
+        public void Class_Should_Implement_IValueConverter_Interface()
+        {
+            var target = new HumanizeTimeSpanValueConverter();
+
+            Assert.IsInstanceOfType(target, typeof(IValueConverter));
+        }
+
+        [TestMethod]
+        public void Convert_DateTime_Should_Return_FriendlyString()
+        {
+            // arrange
+            var target = new HumanizeTimeSpanValueConverter();
+            var now = DateTime.Now;
+
+            // act
+            var result = target.Convert(now, null, null, null);
+
+            // assert
+            Assert.IsInstanceOfType(result, typeof(string));
+            Assert.IsTrue(result.ToString().EndsWith("ago"));
+        }
+
+        [TestMethod]
+        public void Convert_ParameterNotDateTime_Should_Return_EmptyString()
+        {
+            // arrange
+            var target = new HumanizeTimeSpanValueConverter();
+
+            // act
+            var result = target.Convert("hello", null, null, null);
+
+            // assert
+            Assert.IsInstanceOfType(result, typeof(string));
+            Assert.AreEqual(String.Empty, result);
+        }
+
+        [ExpectedException(typeof(NotImplementedException))]
+        [TestMethod]
+        public void ConvertBack_Should_Throw_NotImplementedException()
+        {
+            // arrange
+            var target = new HumanizeTimeSpanValueConverter();
+
+            // act - should throw
+            var result = target.ConvertBack(null, null, null, null);
+        }
+    }
+}
