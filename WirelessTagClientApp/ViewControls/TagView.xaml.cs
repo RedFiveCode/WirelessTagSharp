0d767c0b1c8d32c1776cf1c875e1f0a53efa0e98
John <John@Nectarine>
2022-09-30 19:54:05 +0100

    Move TagCurrentView to ViewControls folder

--- WirelessTagClientApp/ViewControls/TagView.xaml.cs
+++ WirelessTagClientApp/ViewControls/TagView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WirelessTagClientApp.Views
+namespace WirelessTagClientApp.ViewControls
 {
     /// <summary>
     /// Interaction logic for TagView.xaml
