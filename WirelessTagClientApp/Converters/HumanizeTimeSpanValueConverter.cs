17e7e0dcd2c9c0382cce039dcb8e06a5c24fdb3c
John <John@Nectarine>
2022-09-30 20:53:05 +0100

    Show friendly time since last communication in verbose details view

---
+++ WirelessTagClientApp/Converters/HumanizeTimeSpanValueConverter.cs
@@ -0,0 +1,49 @@
+﻿using Humanizer;
+using System;
+using System.Windows.Data;
+
+namespace WirelessTagClientApp.Converters
+{
+    public class HumanizeTimeSpanValueConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        /// <summary>
+        /// Convert
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                var dt = (DateTime)value;
+                var span = DateTime.Now.Subtract(dt);
+
+                return String.Format(Properties.Resources.DurationAgo, span.Humanize());
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
+        /// <summary>
+        /// ConvertBack
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
