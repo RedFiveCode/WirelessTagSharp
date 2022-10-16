cb4f64c3153dc5522f3e98e9c13e59ff32a917d5
John <John@Nectarine>
2022-10-16 12:55:39 +0100

    Add NavigateHyperlinkCommand and unit tests

---
+++ WirelessTagClientApp/Utils/ProcessStarter.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using WirelessTagClientApp.Interfaces;
+
+namespace WirelessTagClientApp.Utils
+{
+    /// <summary>
+    /// Wrapper around System.Diagnostics.Process 
+    /// </summary>
+    public class ProcessStarter : IProcessStarter
+    {
+        public Process Start(string filename)
+        {
+            var info = new ProcessStartInfo(filename);
+            return Process.Start(info);
+        }
+
+        public Process Start(ProcessStartInfo info)
+        {
+            return Process.Start(info);
+        }
+    }
+}
