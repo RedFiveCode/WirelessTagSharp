592c73f18aa700be6f8d4b45809753e7de53299f
John <John@Nectarine>
2022-10-26 20:44:16 +0100

    Added ChangeViewCommand to change active view in main window.

---
+++ WirelessTagClientApp/Commands/ChangeViewCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Input;
+using WirelessTagClientApp.Common;
+using WirelessTagClientApp.ViewModels;
+
+namespace WirelessTagClientApp.Commands
+{
+    /// <summary>
+    /// Command to set view mode on main view view-model
+    /// </summary>
+    public class ChangeViewCommand
+    {
+        private MainWindowViewModel.ViewMode mode;
+
+        /// <summary>
+        /// Get the command object.
+        /// </summary>
+        public ICommand Command { get; private set; }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="mode"></param>
+        public ChangeViewCommand(MainWindowViewModel.ViewMode mode)
+        {
+            this.mode = mode;
+            Command = new RelayCommandT<MainWindowViewModel>(p => ChangeView(p));
+        }
+
+        private void ChangeView(MainWindowViewModel p)
+        {
+            p.Mode = mode; // this will change the active view-model as well
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Commands/ChangeViewCommandTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Input;
+using WirelessTagClientApp.Commands;
+using WirelessTagClientApp.ViewModels;
+
+namespace WirelessTagClientApp.Test.Commands
+{
+    /// <summary>
+    /// Unit tests for the <see cref="ChangeViewCommand"/> command.
+    /// </summary>
+    [TestClass]
+    public class ChangeViewCommandTest
+    {
+        [TestMethod]
+        public void ChangeViewCommand_Implements_ICommand()
+        {
+            var target = new ChangeViewCommand(ViewModels.MainWindowViewModel.ViewMode.SummaryView);
+
+            Assert.IsInstanceOfType(target.Command, typeof(ICommand));
+        }
+
+        [TestMethod]
+        public void ChangeViewCommand_Sets_MainWindowViewModel_Mode()
+        {
+            // arrange
+            var viewModel = new MainWindowViewModel();
+            var target = new ChangeViewCommand(ViewModels.MainWindowViewModel.ViewMode.MinMaxView);
+
+            Assert.AreNotEqual(MainWindowViewModel.ViewMode.MinMaxView, viewModel.Mode);
+
+            // act
+            target.Command.Execute(viewModel);
+
+            // assert
+            Assert.AreEqual(MainWindowViewModel.ViewMode.MinMaxView, viewModel.Mode);
+        }
+    }
+}
