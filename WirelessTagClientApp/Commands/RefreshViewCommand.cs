223f2b5b38e130aac8b7e9ee5e40962f2264fa0d
John <John@Nectarine>
2022-11-08 18:45:59 +0000

    Refactor refreshing main view

---
+++ WirelessTagClientApp/Commands/RefreshViewCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using WirelessTagClientApp.Common;
+using WirelessTagClientApp.ViewModels;
+
+namespace WirelessTagClientApp.Commands
+{
+    /// <summary>
+    /// Command to refresh the main view
+    /// </summary>
+    public class RefreshViewCommand
+    {
+        /// <summary>
+        /// Get the command object.
+        /// </summary>
+        public ICommand Command { get; private set; }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="mode"></param>
+        public RefreshViewCommand()
+        {
+            Command = new RelayCommandT<MainWindowViewModel>(p => Refresh(p), p => CanRefresh(p));
+        }
+
+        private bool CanRefresh(MainWindowViewModel p)
+        {
+            return true;
+        }
+
+        private void Refresh(MainWindowViewModel p)
+        {
+            p.Refresh();
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Commands/RefreshViewCommandTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Input;
+using WirelessTagClientApp.Commands;
+
+namespace WirelessTagClientApp.Test.Commands
+{
+    /// <summary>
+    /// Unit tests for the <see cref="RefreshViewCommand"/> command.
+    /// </summary>
+    [TestClass]
+    public class RefreshViewCommandTest
+    {
+        [TestMethod]
+        public void RefreshViewCommand_Implements_ICommand()
+        {
+            var target = new RefreshViewCommand();
+
+            Assert.IsInstanceOfType(target.Command, typeof(ICommand));
+        }
+    }
+}
