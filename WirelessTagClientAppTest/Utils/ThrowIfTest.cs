87d7abdde0ccb106a30421a79af381ea8e71b507
John <88855545+RedFiveCode@users.noreply.github.com>
2022-11-14 19:02:52 +0000

    Dedicated exception helper class (from https://mariusschulz.com/blog/implementing-an-exception-helper-class-for-parameter-null-checking) Thanks Marius!

---
+++ WirelessTagClientAppTest/Utils/ThrowIfTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WirelessTagClientApp.Utils;
+
+namespace WirelessTagClientApp.Test.Utils
+{
+    /// <summary>
+    /// Unit tests for <see cref="ThrowIf"/> class.
+    /// </summary>
+    [TestClass]
+    public class ThrowIfTest
+    {
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void Argument_IsNull_Throws_ArgumentNullException()
+        {
+            object value = null;
+
+            ThrowIf.Argument.IsNull(value, nameof(value));
+        }
+
+        [TestMethod]
+        public void Argument_IsNull_DoesNot_Throw_Exception()
+        {
+            object value = this;
+
+            ThrowIf.Argument.IsNull(value, nameof(value));
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void Argument_IsNotEqual_Throws_ArgumentException()
+        {
+            bool value = true;
+
+            ThrowIf.Argument.IsNotEqual(value, nameof(value));
+        }
+
+        [TestMethod]
+        public void Argument_IsNotEqual_DoesNot_Throw_Exceptionn()
+        {
+            bool value = false;
+
+            ThrowIf.Argument.IsNotEqual(value, nameof(value));
+        }
+    }
+}
