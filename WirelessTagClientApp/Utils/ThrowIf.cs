9a10947707fe4527c2de70305d3cc54eab2a5622
John <John@Nectarine>
2022-11-14 19:02:52 +0000

    Dedicated exception helper class (from https://mariusschulz.com/blog/implementing-an-exception-helper-class-for-parameter-null-checking) Thanks Marius!

---
+++ WirelessTagClientApp/Utils/ThrowIf.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WirelessTagClientApp.Utils
+{
+    /// <summary>
+    /// Dedicated exception helper class.
+    /// </summary>
+    /// <remarks>
+    /// From http://blog.mariusschulz.com/2014/05/16/implementing-an-exception-helper-class-for-parameter-null-checking
+    /// Thanks Marius!
+    /// </remarks>
+    public static class ThrowIf
+    {
+        public static class Argument
+        {
+            /// <summary>
+            /// Throws an <see cref="ArgumentNullException"/> if the supplied object is null.
+            /// </summary>
+            /// <param name="argument"></param>
+            /// <param name="argumentName"></param>
+            public static void IsNull(object argument, string argumentName)
+            {
+                if (argument == null)
+                {
+                    throw new ArgumentNullException(argumentName);
+                }
+            }
+
+            /// <summary>
+            /// Throws an <see cref="ArgumentException"/> if the supplied object is not equal.
+            /// </summary>
+            /// <param name="argument"></param>
+            /// <param name="argumentName"></param>
+            public static void IsNotEqual(bool argument, string argumentName)
+            {
+                if (argument)
+                {
+                    throw new ArgumentException(argumentName);
+                }
+            }
+        }
+    }
+}
