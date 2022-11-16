f06a6088cc49b9ad1d1bc7ea9600f38167bd9876
John <John@Nectarine>
2022-11-16 19:54:16 +0000

    Add CSVWriter class

---
+++ WirelessTagClientApp/Utils/CSVWriter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WirelessTagClientApp.Utils
+{
+    public class CSVWriter<T>
+    {
+        private string headerDelimiter = "#";
+        private string separator = ",";
+        private string newline = Environment.NewLine;
+
+        private List<Func<T, string>> _expressionList;
+        private string _columnHeader;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSVWriter"/> class.
+        /// </summary>
+        public CSVWriter()
+        {
+            _expressionList = new List<Func<T, string>>();
+            _columnHeader = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSVWriter"/> class.
+        /// </summary>
+        /// <param name="separator">Separator between values; usually comma.</param>
+        /// <param name="newline">Separator between records; usually newline.</param>
+        public CSVWriter(string separator, string newline) : this()
+        {
+            this.separator = separator;
+            this.newline = newline;
+        }
+
+        /// <summary>
+        /// Register an expression (an action or lambda taking an instance of the templated type, and returning a string)
+        /// </summary>
+        /// <remarks>
+        /// Expressions are evaluated in the order added; defines the column order.
+        /// </remarks>
+        /// <param name="expression"></param>
+        public void AddColumn(Func<T, string> expression)
+        {
+            AddColumn(expression, null);
+        }
+
+        /// <summary>
+        /// Register an expression (an action or lambda taking an instance of the templated type, and returning a string)
+        /// </summary>
+        /// <remarks>
+        /// Expressions are evaluated in the order added; defines the column order.
+        /// </remarks>
+        /// <param name="expression"></param>
+        /// <param name="columnName">Optional column name for header.</param>
+        public void AddColumn(Func<T, string> expression, string columnName)
+        {
+            ThrowIf.Argument.IsNull(expression, nameof(expression));
+            _expressionList.Add(expression);
+
+            // append optional column name for header
+            if (columnName != null)
+            {
+                if (_columnHeader == null)
+                {
+                    _columnHeader = headerDelimiter + columnName;
+                }
+                else
+                {
+                    // append separator and next column name
+                    _columnHeader += separator;
+                    _columnHeader += columnName;
+                }
+            }
+        }
+
+        public string WriteCSV(IList<T> list)
+        {
+            ThrowIf.Argument.IsNull(list, nameof(list));
+
+            var builder = new StringBuilder();
+
+            // add optional header
+            if (!String.IsNullOrEmpty(_columnHeader))
+            {
+                builder.Append(_columnHeader);
+                builder.Append(newline);
+            }
+
+            foreach(var item in list)
+            {
+                foreach(var expression in _expressionList)
+                {
+                    var value = expression(item); 
+
+                    if (value == null)
+                    {
+                        value = "(null)";
+                    }
+
+                    // check if value already contains a comma separator
+                    if (value.Contains(separator))
+                    {
+                        value = value.Replace(separator, @"\" + separator);
+                    }
+
+                    builder.Append(value);
+
+                    // append separator for all values except the last
+                    if (expression != _expressionList.Last())
+                    {
+                        builder.Append(separator);
+                    }
+                }
+                builder.Append(newline);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Utils/CSVWriterTest.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WirelessTagClientApp.Utils;
+
+namespace WirelessTagClientApp.Test.Utils
+{
+    [TestClass]
+    public class CSVWriterTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddColumn_Null_Expression_Should_Throw_ArgumentNullException()
+        {
+            var target = new CSVWriter<MyRecord>();
+            target.AddColumn(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void WriteCSV_Null_List_Should_Throw_ArgumentNullException()
+        {
+            var target = new CSVWriter<MyRecord>();
+            target.WriteCSV(null);
+        }
+
+        [TestMethod]
+        public void WriteCSV_Empty_List_Should_Return_Empty_String()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            var target = new CSVWriter<MyRecord>();
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual(String.Empty, result);
+        }
+
+        [TestMethod]
+        public void WriteCSV_Should_Return_String_With_Values_Separated_By_Comma_Except_For_Last_Value()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            list.Add(new MyRecord() { IntValue = 42, DoubleValue = 3.1415, AnotherDoubleValue = 42.1, Text = "hello" });
+
+            var target = new CSVWriter<MyRecord>();
+            target.AddColumn(x => x.IntValue.ToString());
+            target.AddColumn(x => x.DoubleValue.ToString("f2"));
+            target.AddColumn(x => x.AnotherDoubleValue.ToString("f2"));
+            target.AddColumn(x => x.Text);
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual("42,3.14,42.10,hello\r\n", result);
+        }
+
+        [TestMethod]
+        public void WriteCSV_One_Item_In_List_Should_Return_Expected_String()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            list.Add(new MyRecord() { IntValue = 42, DoubleValue = 3.1415, Text = "hello, world!" });
+
+            var target = new CSVWriter<MyRecord>();
+            target.AddColumn(x => x.IntValue.ToString());
+            target.AddColumn(x => x.DoubleValue.ToString("f2"));
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual("42,3.14\r\n", result);
+        }
+
+        [TestMethod]
+        public void WriteCSV_Two_Items_In_List_Should_Return_Expected_String()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            list.Add(new MyRecord() { IntValue = 42, DoubleValue = Math.PI, Text = "hello john" });
+            list.Add(new MyRecord() { IntValue = 43, DoubleValue = Math.E });
+
+            var target = new CSVWriter<MyRecord>();
+            target.AddColumn(x => x.IntValue.ToString());
+            target.AddColumn(x => x.DoubleValue.ToString("f2"));
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual("42,3.14\r\n43,2.72\r\n", result);
+        }
+
+        [TestMethod]
+        public void WriteCSV_With_Special_Delimiters_One_Item_In_List_Should_Return_Expected_String()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            list.Add(new MyRecord() { IntValue = 42, DoubleValue = 3.1415, Text = "hello john" });
+
+            var target = new CSVWriter<MyRecord>("!", " ");
+            target.AddColumn(x => x.IntValue.ToString());
+            target.AddColumn(x => x.DoubleValue.ToString("f2"));
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual("42!3.14 ", result);
+        }
+
+        [TestMethod]
+        public void WriteCSV_With_Value_Containing_Separator_Should_Return_Expected_String_Containing_Delimited_Separator()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            list.Add(new MyRecord() { IntValue = 42, DoubleValue = 3.1415, Text = "hello, world!" });
+
+            var target = new CSVWriter<MyRecord>();
+            target.AddColumn(x => x.IntValue.ToString());
+            target.AddColumn(x => x.Text);
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual("42,hello\\, world!\r\n", result);
+        }
+
+        [TestMethod]
+        public void WriteCSV_WithHeader_One_Item_In_List_Should_Return_Expected_String()
+        {
+            // arrange
+            var list = new List<MyRecord>();
+            list.Add(new MyRecord() { IntValue = 42, DoubleValue = 3.1415, Text = "hello, world!" });
+
+            var target = new CSVWriter<MyRecord>();
+            target.AddColumn(x => x.IntValue.ToString(), "IntValue");
+            target.AddColumn(x => x.DoubleValue.ToString("f2"), "DoubleValue");
+
+            // act
+            var result = target.WriteCSV(list);
+
+            // assert
+            Assert.AreEqual("#IntValue,DoubleValue\r\n42,3.14\r\n", result);
+        }
+    }
+
+    public class MyRecord
+    {
+        public int IntValue { get; set; }
+        public int AnotherValue { get; set; }
+        public double DoubleValue { get; set; }
+        public double AnotherDoubleValue { get; set; }
+        public string Text { get; set; }
+    }
+}
