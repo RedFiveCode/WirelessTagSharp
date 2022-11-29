33e859f9b6ca8c4ebc670ec0e721a617251e09b0
John <88855545+RedFiveCode@users.noreply.github.com>
2022-11-29 19:55:18 +0000

    Add command to copy All Tags view to clipboard

---
+++ WirelessTagClientApp/Commands/CopyAllTagsCommand.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Windows.Input;
+using WirelessTagClientApp.Common;
+using WirelessTagClientApp.Interfaces;
+using WirelessTagClientApp.Utils;
+using WirelessTagClientApp.ViewModels;
+
+namespace WirelessTagClientApp.Commands
+{
+    public class CopyAllTagsCommand
+    {
+        private readonly IClipboardWriter clipboardWriter;
+
+        /// <summary>
+        /// Get the command object.
+        /// </summary>
+        public ICommand Command { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyAllTagsCommand"/> class
+        /// </summary>
+        /// <param name="mode"></param>
+        public CopyAllTagsCommand() : this(new ClipboardWriter())
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyAllTagsCommand"/> class for unit testing
+        /// </summary>
+        /// <param name="clipboardWriter">Clipboard writer</param>
+        public CopyAllTagsCommand(IClipboardWriter clipboardWriter)
+        {
+            Command = new RelayCommandT<AllTagsViewModel>(p => Copy(p), p => CanCopy(p));
+            this.clipboardWriter = clipboardWriter;
+        }
+
+        private bool CanCopy(AllTagsViewModel viewModel)
+        {
+            return viewModel != null && viewModel.Tags != null && viewModel.Tags.Any();
+        }
+
+        private void Copy(AllTagsViewModel viewModel)
+        {
+            if (!CanCopy(viewModel))
+            {
+                return;
+            }
+
+            var csv = GetCSVData(viewModel);
+
+            clipboardWriter.WriteText(csv);
+        }
+
+        private string GetCSVData(AllTagsViewModel viewModel)
+        {
+            var writer = new CSVWriter<TagViewModel>();
+            writer.AddColumn(x => x.Id.ToString(), "Id");
+            writer.AddColumn(x => x.Name, "Name");
+            writer.AddColumn(x => x.Description, "Description");
+            writer.AddColumn(x => x.Uuid.ToString(), "Uuid");
+            writer.AddColumn(x => x.Temperature.ToString("f1"), "Temperature (C)");           
+            writer.AddColumn(x => x.RelativeHumidity.ToString("f1"), "RelativeHumidity");
+            writer.AddColumn(x => x.SignalStrength.ToString(), "SignalStrength (dBm)");
+            writer.AddColumn(x => x.BatteryVoltage.ToString("f1"), "BatteryVoltage (V)");
+            writer.AddColumn(x => x.BatteryRemaining.ToString("f1"), "BatteryRemaining (%)");
+            writer.AddColumn(x => x.LastCommunication.ToString("dd-MMM-yyyy HH:mm:ss"), "Last Communication");
+            writer.AddColumn(x => x.IsHumidityTag.ToString(), "IsHumidityTag");
+
+            return writer.WriteCSV(viewModel.Tags);
+        }
+    }
+}
