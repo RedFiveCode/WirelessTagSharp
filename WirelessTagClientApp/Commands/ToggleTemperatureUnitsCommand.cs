e5fd6840068f78989f718dd8f42a69be97722605
John <88855545+RedFiveCode@users.noreply.github.com>
2024-04-17 19:20:06 +0100

    Add ToggleTemperatureUnitsCommand

---
+++ WirelessTagClientApp/Commands/ToggleTemperatureUnitsCommand.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using WirelessTagClientApp.Common;
+using WirelessTagClientApp.ViewModels;
+
+namespace WirelessTagClientApp.Commands
+{
+    public class ToggleTemperatureUnitsCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToggleTemperatureUnitsCommand"/>
+        /// </summary>
+        public ToggleTemperatureUnitsCommand()
+        {
+            Command = new RelayCommandT<MinMaxViewModel>(p => ChangeUnits(p), p => CanChangeUnits(p));
+        }
+
+        /// <summary>
+        /// Get the command object.
+        /// </summary>
+        public ICommand Command { get; private set; }
+
+        private bool CanChangeUnits(MinMaxViewModel viewModel)
+        {
+            return true; // can always change the units
+        }
+
+        private void ChangeUnits(MinMaxViewModel viewModel)
+        {
+            if (CanChangeUnits(viewModel))
+            {
+                viewModel.TemperatureUnits = viewModel.IsTemperatureCelsius ? TemperatureUnits.Farenheit : TemperatureUnits.Celsius;
+            }
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Commands/ToggleTemperatureUnitsCommandTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Input;
+using WirelessTagClientApp.Commands;
+using WirelessTagClientApp.ViewModels;
+
+namespace WirelessTagClientApp.Test.Commands
+{
+    [TestClass]
+    public class ToggleTemperatureUnitsCommandTest
+    {
+        [TestMethod]
+        public void ToggleViewCommand_Implements_ICommand()
+        {
+            var target = new ToggleTemperatureUnitsCommand();
+
+            Assert.IsInstanceOfType(target.Command, typeof(ICommand));
+        }
+
+        [TestMethod]
+        public void ToggleViewCommand_Celsius_SetsUnitsToFahrenheit()
+        {
+            // arrange
+            var target = new ToggleTemperatureUnitsCommand();
+            var viewModel = new MinMaxViewModel()
+            {
+                TemperatureUnits = TemperatureUnits.Celsius
+            };
+
+            // act
+            target.Command.Execute(viewModel);
+
+            // assert
+            Assert.AreEqual(TemperatureUnits.Farenheit, viewModel.TemperatureUnits);
+        }
+
+        [TestMethod]
+        public void ToggleViewCommand_Fahrenheit_SetsUnitsToCelsius()
+        {
+            // arrange
+            var target = new ToggleTemperatureUnitsCommand();
+            var viewModel = new MinMaxViewModel()
+            {
+                TemperatureUnits = TemperatureUnits.Farenheit
+            };
+
+            // act
+            target.Command.Execute(viewModel);
+
+            // assert
+            Assert.AreEqual(TemperatureUnits.Celsius, viewModel.TemperatureUnits);
+        }
+    }
+}
