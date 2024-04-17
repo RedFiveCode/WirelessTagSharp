5cd18a66a02c1632da8223b22aba9a53336e048b
John <88855545+RedFiveCode@users.noreply.github.com>
2024-04-17 22:06:40 +0100

    ColumnWidthConverter from https://highfieldtales.wordpress.com/2013/08/05/hacking-gridview-hide-columns with local changes and unit tests

---
+++ WirelessTagClientApp/Converters/ColumnWidthConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WirelessTagClientApp.Converters
+{
+    /// <summary>
+    /// This converter targets a column header, in order to take its width to zero when it should be hidden.
+    /// </summary>
+    /// <remarks>
+    /// https://highfieldtales.wordpress.com/2013/08/05/hacking-gridview-hide-columns/
+    /// </remarks>
+    public class ColumnWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                var isVisible = (bool)value;
+
+                if (parameter != null)
+                {
+                    if (double.TryParse(parameter.ToString(), out double width))
+                    {
+                        return isVisible ? width : 0d;
+                    }
+                }
+            }
+            return 0d;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Converters/ColumnWidthConverterTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Data;
+using WirelessTagClientApp.Converters;
+
+namespace WirelessTagClientApp.Test.Converters
+{
+    [TestClass]
+    public class ColumnWidthConverterTest
+    {
+        [TestMethod]
+        public void Class_Should_Implement_IValueConverter_Interface()
+        {
+            var target = new ColumnWidthConverter();
+
+            Assert.IsInstanceOfType(target, typeof(IValueConverter));
+        }
+
+        [TestMethod]
+        public void Convert_True_Should_Return_ParameterisedWidth()
+        {
+            // arrange
+            var target = new ColumnWidthConverter();
+
+            // act
+            var result = target.Convert(true, null, 100d, null);
+
+            // assert
+            Assert.AreEqual(100d, result);
+        }
+
+        [TestMethod]
+        public void Convert_False_Should_Return_Zero()
+        {
+            // arrange
+            var target = new ColumnWidthConverter();
+
+            // act
+            var result = target.Convert(false, null, 100d, null);
+
+            // assert
+            Assert.AreEqual(0d, result);
+        }
+
+        [TestMethod]
+        public void Convert_ValueNotBoolean_Should_Return_Zero()
+        {
+            // arrange
+            var target = new ColumnWidthConverter();
+
+            // act
+            var result = target.Convert("hello", null, 100d, null);
+
+            // assert
+            Assert.AreEqual(0d, result);
+        }
+
+        [TestMethod]
+        public void Convert_ParameterNotDouble_Should_Return_Zero()
+        {
+            // arrange
+            var target = new ColumnWidthConverter();
+
+            // act
+            var result = target.Convert(false, null, "hello", null);
+
+            // assert
+            Assert.AreEqual(0d, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void ConvertBack_Should_Throw_NotImplementedException()
+        {
+            // arrange
+            var target = new ColumnWidthConverter();
+
+            // act - should throw
+            var result = target.ConvertBack(null, null, null, null);
+        }
+    }
+}
