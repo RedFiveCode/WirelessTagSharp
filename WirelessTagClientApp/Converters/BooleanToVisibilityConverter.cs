c88780fa47bdcefd5ec7cdc7ae924549ce5a11ac
John <88855545+RedFiveCode@users.noreply.github.com>
2024-04-17 19:18:40 +0100

    Add BooleanToVisibilityConverter value converter

---
+++ WirelessTagClientApp/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WirelessTagClientApp.Converters
+{
+    /// <summary>
+    /// ValueConverter to convert a boolean to a visibility enumeration.
+    /// </summary>
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Get/set the <see cref="Visibility"/> to use if the converter value is true.
+        /// </summary>
+        public Visibility VisibilityTrue { get; set; }
+
+        /// <summary>
+        /// Get/set the <see cref="Visibility"/> to use if the converter value is false.
+        /// </summary>        
+        public Visibility VisibilityFalse { get; set; }
+
+        /// <summary>
+        /// <see cref="BoolVisibilityConverter"/> constructor.
+        /// </summary>
+        public BooleanToVisibilityConverter()
+        {
+            VisibilityTrue = Visibility.Visible;
+            VisibilityFalse = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Convert
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = false;
+
+            if (value is bool)
+            {
+                boolValue = (bool)value;
+            }
+
+            return boolValue ? VisibilityTrue : VisibilityFalse;    
+        }
+
+        /// <summary>
+        /// ConvertBack
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WirelessTagClientAppTest/Converters/BooleanToVisibilityConverterTest.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows;
+using System.Windows.Data;
+using WirelessTagClientApp.Converters;
+
+namespace WirelessTagClientApp.Test.Converters
+{
+    [TestClass]
+    public class BooleanToVisibilityConverterTest
+    {
+        [TestMethod]
+        public void Class_Should_Implement_IValueConverter_Interface()
+        {
+            var target = new BooleanToVisibilityConverter();
+
+            Assert.IsInstanceOfType(target, typeof(IValueConverter));
+        }
+
+        [TestMethod]
+        public void Convert_True_Should_Return_Visible()
+        {
+            // arrange
+            var target = new BooleanToVisibilityConverter();
+            bool value = true;
+
+            // act
+            var result = target.Convert(value, null, null, null);
+
+            // assert
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        [TestMethod]
+        public void Convert_False_Should_Return_Collapsed()
+        {
+            // arrange
+            var target = new BooleanToVisibilityConverter();
+            bool value = false;
+
+            // act
+            var result = target.Convert(value, null, null, null);
+
+            // assert
+            Assert.AreEqual(Visibility.Collapsed, result);
+        }
+
+        [TestMethod]
+        public void Convert_Inverted_True_Should_Return_Visible()
+        {
+            // arrange
+            var target = new BooleanToVisibilityConverter()
+            { 
+                VisibilityTrue = Visibility.Hidden,
+                VisibilityFalse = Visibility.Visible
+            };
+
+            bool value = true;
+
+            // act
+            var result = target.Convert(value, null, null, null);
+
+            // assert
+            Assert.AreEqual(Visibility.Hidden, result);
+        }
+
+        [TestMethod]
+        public void Convert_Inverted_False_Should_Return_Collapsed()
+        {
+            // arrange
+            var target = new BooleanToVisibilityConverter()
+            {
+                VisibilityTrue = Visibility.Hidden,
+                VisibilityFalse = Visibility.Visible
+            };
+
+            bool value = false;
+
+            // act
+            var result = target.Convert(value, null, null, null);
+
+            // assert
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        [TestMethod]
+        public void Convert_ParameterNotBooleanValue_Should_Return_Collapsed()
+        {
+            // arrange
+            var target = new BooleanToVisibilityConverter();
+
+            // act
+            var result = target.Convert(123, null, null, null);
+
+            // assert
+            Assert.AreEqual(Visibility.Collapsed, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void ConvertBack_Should_Throw_NotImplementedException()
+        {
+            // arrange
+            var target = new BooleanToVisibilityConverter();
+
+            // act - should throw
+            var result = target.ConvertBack(null, null, null, null);
+        }
+    }
+}
