ba52570f5cf2a8250f3797d73af41d316028ce8a
John <88855545+RedFiveCode@users.noreply.github.com>
2025-07-29 20:50:59 +0100

    Added DateTimeChunker class and unit tests

---
+++ WirelessTagClientLib.Test/Utils/DateTimeChunkerTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace WirelessTagClientLib.Test.Utils
+{
+    public class DateTimeChunkerTest
+    {
+        [Fact]
+        public void SplitDateTimeRange_From_After_To_ThrowsArgumentException()
+        {
+            // arrange
+            var from = DateTime.Now;
+            var to = from.AddDays(-10);
+
+            // act; should throw, use ToList to force enumeration, otherwise not evaluated
+            Assert.Throws<ArgumentException>(() => DateTimeChunker.SplitDateTimeRange(from, to, TimeSpan.FromDays(1)).ToList());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void SplitDateTimeRange_Span_LessThanZero_ThrowsArgumentOutOfRangeException(int span)
+        {
+            // arrange
+            var from = DateTime.Now;
+            var to = from.AddDays(10);
+
+            // act; should throw, use ToList to force enumeration, otherwise not evaluated
+            Assert.Throws<ArgumentOutOfRangeException>(() => DateTimeChunker.SplitDateTimeRange(from, to, TimeSpan.FromDays(span)).ToList());
+        }
+
+        [Fact]
+        public void SplitDateTimeRange_Wide_Returns_ExpectedItems()
+        {
+            // arrange
+            var from = new DateTime(2025, 1, 1, 10, 11, 12);
+            var to = new DateTime(2025, 1, 12, 10, 11, 12);
+
+            // act
+            var results =  DateTimeChunker.SplitDateTimeRange(from, to, TimeSpan.FromDays(5));
+
+            // assert
+            var resultsList = results.ToList();
+
+            Assert.Equal(3, resultsList.Count); // 2 chunks of 5 days each plus remainder
+
+            var start = new DateTime(2025, 1, 1);
+            AssertDateTimeRange(resultsList[0], new DateTime(2025, 1, 1), new DateTime(2025, 1, 5, 23, 59, 59).AddTicks(9999999L));
+            AssertDateTimeRange(resultsList[1], new DateTime(2025, 1, 6), new DateTime(2025, 1, 10, 23, 59, 59).AddTicks(9999999L));
+            AssertDateTimeRange(resultsList[2], new DateTime(2025, 1, 11), new DateTime(2025, 1, 12, 23, 59, 59).AddTicks(9999999L));
+        }
+
+        [Fact]
+        public void SplitDateTimeRange_Narrow_Returns_ExpectedItems()
+        {
+            // arrange
+            var from = new DateTime(2025, 1, 1, 10, 11, 12);
+            var to = new DateTime(2025, 1, 3, 10, 11, 12);
+
+            // act
+            var results = DateTimeChunker.SplitDateTimeRange(from, to, TimeSpan.FromDays(5));
+
+            // assert
+            var resultsList = results.ToList();
+
+            Assert.Single(resultsList); // only one chunk since the range is less than 5 days
+
+            var start = new DateTime(2025, 1, 1);
+            AssertDateTimeRange(resultsList[0], new DateTime(2025, 1, 1), new DateTime(2025, 1, 3, 23, 59, 59).AddTicks(9999999L));
+        }
+
+        private void AssertDateTimeRange((DateTime Start, DateTime End) result, DateTime expectedStart, DateTime expectedEnd)
+        {
+            Assert.Equal(expectedStart, result.Start);
+            Assert.Equal(expectedEnd, result.End);
+        }
+    }
+}
---
+++ WirelessTagClientLib/Utils/DateTimeChunker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WirelessTagClientLib
+{
+    /// <summary>
+    /// DateTimeChunker
+    /// </summary>
+    public class DateTimeChunker
+    {
+        public static IEnumerable<(DateTime Start, DateTime End)> Foo()
+        {
+            throw new ArgumentException();
+        }
+
+        /// <summary>
+        /// Returns a date range split into chunks of a maximum size
+        /// </summary>
+        /// <remarks>
+        /// In the return tuple, the start value is rounded down to midnight (00:00:00),
+        /// and the end value is rounded up to just before midnight (23:59:59).
+        /// </remarks>
+        /// <param name="from">From date</param>
+        /// <param name="to">To date</param>
+        /// <param name="interval">Maximum chunk size, typically days</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="from"/> is after <paramref name="to"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="interval"/> is less than or equal to zero</exception>
+        public static IEnumerable<(DateTime Start, DateTime End)> SplitDateTimeRange(DateTime from, DateTime to, TimeSpan interval)
+        {
+            if (from > to)
+            {
+                throw new ArgumentException("'from' date must be less than or equal to 'to' date", nameof(from));
+            }
+
+            if (interval <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException("Interval must be greater than zero", nameof(interval));
+            }
+
+            var currentStart = from.Date;
+            var finish = to.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            while (currentStart <= finish)
+            {
+                var currentEnd = currentStart.Add(interval).AddDays(-1); // inclusive range
+
+                // If the calculated end exceeds the 'to' date, use 'to' as the end
+                if (currentEnd > finish)
+                {
+                    currentEnd = finish;
+                }
+
+                // Round up the end time to 23:59:59.9999999 (end of day)
+                currentEnd = currentEnd.Date.AddDays(1).AddTicks(-1);
+
+                yield return (currentStart, currentEnd);
+
+                // Move to the next chunk start
+                currentStart = currentEnd.AddTicks(1); // Add 1 tick to avoid overlap
+            }
+        }
+    }
+}
