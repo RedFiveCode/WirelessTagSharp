ccc8e0845d08e1b4f002a2625bd078baf5a2e37d
John <88855545+RedFiveCode@users.noreply.github.com>
2025-08-03 16:04:11 +0100

    Added cache reader class

---
+++ WirelessTagClientLib.Test/Client/CacheReaderTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using Newtonsoft.Json;
+using WirelessTagClientLib.Client;
+using WirelessTagClientLib.DTO;
+using Xunit;
+
+namespace WirelessTagClientLib.Test.Client
+{
+    public class CacheReaderTest
+    {
+        private readonly MockFileSystem _mockfileSystem;
+        private readonly TagInfo _tagInfo;
+        private readonly CacheReader _sut;
+
+        public CacheReaderTest()
+        {
+            _mockfileSystem = new MockFileSystem();
+            _mockfileSystem.AddDirectory(@"C:\root");
+
+            _tagInfo = new TagInfo
+            {
+                SlaveId = 1,
+                Name = "TestTag",
+                Uuid = Guid.Parse("11111111-1111-1111-1111-111111111111")
+            };
+
+            _sut = new CacheReader(_mockfileSystem);
+        }
+
+        [Fact]
+        public void ReadCache_FolderNull_ThrowsArgumentNullException()
+        {
+            // act; should throw
+            Assert.Throws<ArgumentNullException>(() => _sut.ReadCache(null, _tagInfo));
+        }
+
+        [Fact]
+        public void ReadCache_TagNull_ThrowsArgumentNullException()
+        {
+            // act; should throw
+            Assert.Throws<ArgumentNullException>(() => _sut.ReadCache(@"C:\root\subFolder", null));
+        }
+
+        [Fact]
+        public void ReadCache_FolderNotExist_ThrowsDirectoryNotFoundExceptionn()
+        {
+            // act; should throw
+            Assert.Throws<DirectoryNotFoundException>(() => _sut.ReadCache(@"C:\root\DoesNotExist", _tagInfo));
+        }
+
+        [Fact]
+        public void ReadCache_CacheFileExists_Returns_ExpectedList()
+        {
+            // arrange
+            var data = new List<Measurement>
+            {
+                new Measurement(new DateTime(2025, 1, 1, 12, 0, 0), -2d, 45d, -1, 2.95d),
+                new Measurement(new DateTime(2025, 1, 1, 12, 15, 0), 2.5d, 45d, -1, 2.9d)
+            };
+
+            var json = SerialiseData(data);
+
+            _mockfileSystem.AddDirectory(@"C:\root\subFolder");
+            _mockfileSystem.AddFile(@"C:\root\subFolder\11111111-1111-1111-1111-111111111111.cache.json", new MockFileData(json));
+
+            // act
+            var results = _sut.ReadCache(@"C:\root\subFolder", _tagInfo);
+
+            // assert
+            Assert.NotNull(results);
+            Assert.Equal(2, results.Count);
+
+            Assert.Equal(data[0].Time, results[0].Time);
+            Assert.Equal(data[0].Temperature, results[0].Temperature);
+
+            Assert.Equal(data[1].Time, results[1].Time);
+            Assert.Equal(data[1].Temperature, results[1].Temperature);
+        }
+
+        private static string SerialiseData(List<Measurement> data)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                Formatting = Formatting.Indented
+            };
+
+            return JsonConvert.SerializeObject(data, settings);
+        }
+
+        [Fact]
+        public void ReadCache_CacheFileDoesNotExist_Returns_EmptyList()        
+        {
+            // arrange
+            _mockfileSystem.AddDirectory(@"C:\root\subFolder");
+
+            // act
+            var results = _sut.ReadCache(@"C:\root\subFolder", _tagInfo);
+
+            // assert
+            Assert.NotNull(results);
+            Assert.Empty(results);
+        }
+    }
+}
---
+++ WirelessTagClientLib/Client/CacheReader.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using WirelessTagClientLib.DTO;
+
+namespace WirelessTagClientLib.Client
+{
+    /// <summary>
+    /// CacheReader is responsible for reading cached temperature measurement data for a specific tag from a cache file/folder
+    /// </summary>
+    public class CacheReader
+    {
+        private readonly IFileSystem _fileSystem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheReader"/> class
+        /// </summary>
+        public CacheReader() : this (new FileSystem())
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheReader"/> class for unit testing
+        /// </summary>
+        /// <param name="fileSystem"></param>
+        public CacheReader(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        /// <summary>
+        /// Reads the cache for a specific tag from the specified folder.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        public List<Measurement> ReadCache(string folder, TagInfo tag)
+        {
+            if (string.IsNullOrWhiteSpace(folder))
+            {
+                throw new ArgumentNullException(nameof(folder), "Folder cannot be null or empty");
+            }
+            ThrowIf.Argument.IsNull(tag, nameof(tag));
+
+            if (!_fileSystem.Directory.Exists(folder))
+            {
+                throw new DirectoryNotFoundException($"Cache folder '{folder}' does not exist.");
+            }
+
+            var filename = GetCacheFilename(folder, tag);
+
+            if (!_fileSystem.File.Exists(filename))
+            {
+                return new List<Measurement>(); // empty list
+            }
+
+            var json = _fileSystem.File.ReadAllText(filename);
+
+            var settings = new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                Formatting = Formatting.Indented
+            };
+
+            return JsonConvert.DeserializeObject<List<Measurement>>(json, settings);
+        }
+
+        private string GetCacheFilename(string folder, TagInfo tag)
+        {
+            if (tag == null)
+            {
+                throw new ArgumentNullException(nameof(tag), "Tag cannot be null");
+            }
+
+            // Create a filename based on the tag's UUID and the date range
+            var filename = $"{tag.Uuid}.cache.json";
+
+            return Path.Combine(folder, filename);
+        }
+    }
+}
